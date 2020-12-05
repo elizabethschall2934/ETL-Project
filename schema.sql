08d356e90dfa565d5a6413c23bf30d2e6c75017c
elizabethschall2934 <elizabeth.schall2934@gmail.com>
2020-12-05 14:18:36 -0700

    finalized code and files

--- schema.sql
+++ schema.sql
@@ -1,73 +1,38 @@
 -- Create Tables
-
-CREATE TABLE CA_final (
-	video_id TEXT, 
-	trending_date VARCHAR(60), 
-	title TEXT, 
-	channel_title TEXT,
-	category_id INT PRIMARY KEY,
-	publish_time VARCHAR(60), 
-	tags TEXT, 
-	views INTEGER NOT NULL, 
-	likes INTEGER NOT NULL, 
-	dislikes INTEGER NOT NULL, 
-	comment_count INTEGER NOT NULL,
-	category TEXT,
-	country TEXT
-	);
-	
-	SELECT * FROM CA_final
-	
-CREATE TABLE GB_final (
-	video_id TEXT, 
-	trending_date VARCHAR(60), 
-	title TEXT, 
-	channel_title TEXT,
-	category_id INT PRIMARY KEY,
-	publish_time VARCHAR(60), 
-	tags TEXT, 
-	views INTEGER NOT NULL, 
-	likes INTEGER NOT NULL, 
-	dislikes INTEGER NOT NULL, 
-	comment_count INTEGER NOT NULL,
-	category TEXT,
-	country TEXT
-	);
-	
-	SELECT * FROM GB_final
-	
-	
-CREATE TABLE US_final (
-	video_id TEXT, 
-	trending_date VARCHAR(60), 
-	title TEXT, 
-	channel_title TEXT,
-	category_id INT PRIMARY KEY,
-	publish_time VARCHAR(60), 
-	tags TEXT, 
-	views INTEGER NOT NULL, 
-	likes INTEGER NOT NULL, 
-	dislikes INTEGER NOT NULL, 
-	comment_count INTEGER NOT NULL,
-	category TEXT,
-	country TEXT
-	);
-	
-	SELECT * FROM US_final
-
 CREATE TABLE Category_json (
 	category_id INT PRIMARY KEY,
 	category TEXT 
 	);
 	
 	SELECT * FROM Category_json
-	
-CREATE TABLE World_final (
+
+CREATE TABLE CA_final (
+	unique_id INT PRIMARY KEY,
 	video_id TEXT, 
 	trending_date VARCHAR(60), 
 	title TEXT, 
 	channel_title TEXT,
-	category_id INT PRIMARY KEY,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT,
+	FOREIGN KEY(category_id) REFERENCES Category_json (category_id)
+	);
+	
+	SELECT * FROM CA_final
+	
+CREATE TABLE GB_final (
+	unique_id INT PRIMARY KEY,
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
 	publish_time VARCHAR(60), 
 	tags TEXT, 
 	views INTEGER NOT NULL, 
@@ -76,6 +41,47 @@ CREATE TABLE World_final (
 	comment_count INTEGER NOT NULL,
 	category TEXT,
 	country TEXT
+	FOREIGN KEY(category_id) REFERENCES Category_json (category_id)
+	);
+	
+	SELECT * FROM GB_final
+	
+CREATE TABLE US_final (
+	unique_id INT PRIMARY KEY,
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT
+	FOREIGN KEY(category_id) REFERENCES Category_json (category_id)
+	);
+	
+	SELECT * FROM US_final
+	
+CREATE TABLE World_final (
+	unique_id INT PRIMARY KEY,
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT
+	FOREIGN KEY(category_id) REFERENCES Category_json (category_id)
 	);
 	
 	SELECT * FROM World_final
