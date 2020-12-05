3fb4cc041be85315e5926ca49dab9a9ca43c0870
elizabethschall2934 <elizabeth.schall2934@gmail.com>
2020-12-05 10:38:05 -0700

    sql info added

---
+++ schema.sql
@@ -0,0 +1,82 @@
+-- Create Two Tables
+
+CREATE TABLE CA_final (
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT
+	);
+	
+	SELECT * FROM CA_final
+	
+	CREATE TABLE GB_final (
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT
+	);
+	
+	SELECT * FROM GB_final
+	
+	
+CREATE TABLE US_final (
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT
+	);
+	
+	SELECT * FROM US_final
+
+CREATE TABLE Category_json (
+	category_id INT PRIMARY KEY,
+	category TEXT 
+	);
+	
+	SELECT * FROM Category_json
+	
+	CREATE TABLE world_final (
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT
+	);
+	
+	SELECT * FROM world_final
+	
